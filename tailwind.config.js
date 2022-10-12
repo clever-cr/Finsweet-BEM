f82058e84ae014da6b239c2dcb26039bd347d067
clever <cleverumuhuza@gmail.com>
2022-10-12 10:45:39 +0200

    implement BEM structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['**.html'],
   theme: {
-    extend: {},
+
+    extend: {
+      colors: {
+       primary: "#1D2130",
+       secondary:"#E0E4FC",
+       light:"#D798E1",
+      from:"#373FFF",
+      to: "#3ACAF8"
+      }
+    },
   },
   plugins: [],
 }
